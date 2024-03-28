f6d5290612df947ca7b268d29ca248c6b3e115c3
Seacrest <seacrest@ukr.net>
2024-03-29 01:53:14 +0200

    step 1

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,16 @@ mod db;
 mod models;
 mod ui;
 mod io;
+mod nav;
+
+use std::rc::Rc;
+
+use db::JiraDatabase;
+use nav::Navigator;
 
 use crate::models::*;
 
 fn main() {
-    let x = Epic::new(String::from("foo"), String::from("bar"));
+    let db = Rc::new(JiraDatabase::new("./data/database.json"));
+    let nvigator = Navigator::new(Rc::clone(&db));
 }
